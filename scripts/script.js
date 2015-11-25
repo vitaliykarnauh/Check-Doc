6261c0af3e9fed54de1f4aa293fec2444a77a5f7
Mary <solodovnik.my@gmail.com>
2015-11-25 13:44:14 +0200

    fixed filename bug

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,14 @@
-$(document).ready(function(){
-    $('[data-toggle="tooltip"]').tooltip({
-        placement: 'top'
-    });   
-    $("#header").load("header.html"); 
-/* end dot nav */
+$(document).ready(function (){
+	'use scrict'
+
+    // $('[data-toggle="tooltip"]').tooltip({
+    //     placement: 'top'
+    // });
+    $("#header").load("header.html");
+    $('#file-upload').change(function() {
+    	$full_path = $('#file-upload').val();
+    	partials = $full_path.split('\\');
+		$file_name = partials[partials.length - 1];	
+		$('.file-upload-name').val($file_name);
+    });
 });
